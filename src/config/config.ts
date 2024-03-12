0c9a9e021c54d4d1e9d1403b466934c56e0f8ece
papaibrahima12 <papaibrahimasow@esp.sn>
2024-03-12 11:42:49 +0000

    hide config

--- src/config/config.ts
+++
@@ -1,4 +0,0 @@
-export const secretKey = {
-    secret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9fJKAJKLFAfsuiwegnvjkesdjeknsd',
-    accessKey: '0a6b944d-d2fb-46fc-a85e-0295c98mldiaiwehdwaduyq23231232432Afsuiwegnvjkesdjeknsd'
-  };
